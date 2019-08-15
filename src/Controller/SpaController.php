b22d1349b80846ffe3af27c154284962ca72d38c
Anddroid97 <litkovskiy97@gmail.com>
2019-08-15 20:15:54 +0300

    Make spa controller

---
+++ src/Controller/SpaController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SpaController extends AbstractController
+{
+    /**
+     * @Route(path="/{spa_route}", name="spa", requirements={ "spa_route" = ".*" })
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('spa/spa.html.twig');
+    }
+}
