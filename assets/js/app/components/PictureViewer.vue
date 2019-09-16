760af1e79f5019adc318e6b629505b9fd2e98ea9
Anddroid97 <litkovskiy97@gmail.com>
2019-09-16 10:18:12 +0300

    Try to replace markup

---
+++ assets/js/app/components/PictureViewer.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="overlay">
+        <div class="crosshair">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20 1.17716L18.8406 0.0202637L10 8.84396L1.15941 0.0202637L0 1.17716L8.84059 10.0009L0 18.824L1.15941 19.9808L10 11.1578L18.8406 19.9808L20 18.824L11.1588 10.0009L20 1.17716Z"
+                      fill="#03503B"/>
+            </svg>
+        </div>
+        <div class="img_content">
+            <div class="big_image">
+                <img src="../images/012_o.png" alt="">
+            </div>
+            <div class="small_and_text">
+                <div class="small_images">
+                    <img src="../images/003-detail-Wind-o.png" alt="">
+                    <img src="../images/004-detail-Wind-o.png" alt="">
+                </div>
+                <div class="overlay_text">
+                    <div class="overlay_header">
+                        <span>ARTIST STATEMENT</span>
+                    </div>
+                    <div class="overlay_info">
+                        Creating my art I use an opportunity to reflect nature surrounding me and to explore the
+                        possibilities of used materials (thread, cardboard, polyurethane foam, wood, glass,
+                        etc.) through different methods of their application. My work Wind was created by
+                        capturing a moment on a last winter day. The tree-lined view includes no figures, but
+                        gives an easy feeling and understanding of the picture.
+                        Wind is an example of my understanding of nature; attempt to visualize the image of the
+                        wind using color, texture, and form and to explore the range of possibilities allowed
+                        through of the used materials. I think that all my pictures are not made with theory
+                        alone; the materials are the vital component. In my pictures I create inspiration from
+                        undefined feeling of the universe.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PictureViewer"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ assets/js/app/views/Contacts.vue
@@ -0,0 +1,334 @@
+<template>
+    <div class="content">
+        <div class="left">
+            <div class="img">
+                <img src="../images/15.png" alt="icon">
+            </div>
+            <div class="text">
+                The work does not require any special installation
+                Any work of Olena Chornohuz never have been exhibited in the Greater Toronto Area before
+            </div>
+            <div class="feedback_img">
+                <img src="../images/priziv.png" alt="">
+            </div>
+        </div>
+        <div class="right">
+            <div class="cards">
+                <div class="card1">
+                    <div class="svg">
+                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M37.0209 29.9791C35.1693 28.1276 32.9654 26.7569 30.5608 25.9272C33.1362 24.1534 34.8281 21.1848 34.8281 17.8281C34.8281 12.4089 30.4192 8 25 8C19.5808 8 15.1719 12.4089 15.1719 17.8281C15.1719 21.1848 16.8638 24.1534 19.4393 25.9272C17.0347 26.7569 14.8308 28.1276 12.9792 29.9791C9.76833 33.1901 8 37.4591 8 42H10.6562C10.6562 34.0908 17.0908 27.6562 25 27.6562C32.9092 27.6562 39.3438 34.0908 39.3438 42H42C42 37.4591 40.2317 33.1901 37.0209 29.9791ZM25 25C21.0454 25 17.8281 21.7828 17.8281 17.8281C17.8281 13.8735 21.0454 10.6562 25 10.6562C28.9546 10.6562 32.1719 13.8735 32.1719 17.8281C32.1719 21.7828 28.9546 25 25 25Z"
+                                  fill="#03503B"/>
+                            <rect x="1.5" y="1.5" width="47" height="47" stroke="#03503B" stroke-width="3"/>
+                        </svg>
+
+                    </div>
+                    <div class="info">
+                        <div class="card_name">
+                            Имя
+                        </div>
+                        <div class="card_number">
+                            +38(000)000-00-00
+                        </div>
+                    </div>
+                </div>
+                <div class="card2">
+                    <div class="svg">
+                        <svg width="50" height="50" viewBox="0 0 50 50" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M37.0209 29.9791C35.1693 28.1276 32.9654 26.7569 30.5608 25.9272C33.1362 24.1534 34.8281 21.1848 34.8281 17.8281C34.8281 12.4089 30.4192 8 25 8C19.5808 8 15.1719 12.4089 15.1719 17.8281C15.1719 21.1848 16.8638 24.1534 19.4393 25.9272C17.0347 26.7569 14.8308 28.1276 12.9792 29.9791C9.76833 33.1901 8 37.4591 8 42H10.6562C10.6562 34.0908 17.0908 27.6562 25 27.6562C32.9092 27.6562 39.3438 34.0908 39.3438 42H42C42 37.4591 40.2317 33.1901 37.0209 29.9791ZM25 25C21.0454 25 17.8281 21.7828 17.8281 17.8281C17.8281 13.8735 21.0454 10.6562 25 10.6562C28.9546 10.6562 32.1719 13.8735 32.1719 17.8281C32.1719 21.7828 28.9546 25 25 25Z"
+                                  fill="#03503B"/>
+                            <rect x="1.5" y="1.5" width="47" height="47" stroke="#03503B" stroke-width="3"/>
+                        </svg>
+
+                    </div>
+                    <div class="info">
+                        <div class="card_name">
+                            Имя
+                        </div>
+                        <div class="card_number">
+                            +38(000)000-00-00
+                        </div>
+                    </div>
+                </div>
+                <div class="card3">
+                    <div class="svg">
+                        <svg width="40" height="40" viewBox="0 0 40 40" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M39.9723 9.89318C39.9706 8.76435 39.0518 7.84717 37.9233 7.84717C37.9224 7.84717 37.9214 7.84717 37.9206 7.84717L2.04609 7.89646C1.49883 7.89717 0.984531 8.111 0.598047 8.4985C0.211642 8.88607 -0.000701383 9.40099 1.74058e-06 9.9481L0.0277361 30.1066C0.0284392 30.6539 0.242267 31.1682 0.629766 31.5546C1.01656 31.9404 1.53023 32.1527 2.07656 32.1527C2.0775 32.1527 2.07851 32.1527 2.07929 32.1527L37.9538 32.1034C39.0833 32.1018 40.0012 31.1816 39.9998 30.0517L39.9723 9.89318ZM35.6727 9.89912L20.0015 20.7932L4.29999 9.94224L35.6727 9.89912ZM26.6285 22.8492L35.9231 30.0574L4.05038 30.1011L13.4016 22.8492C13.8486 22.5024 13.9301 21.8589 13.5833 21.4118C13.2365 20.9647 12.5931 20.8833 12.1459 21.23L2.07508 29.0401L2.05008 10.878L19.4207 22.8825C19.596 23.0036 19.7995 23.0641 20.0031 23.0641C20.2075 23.0641 20.4122 23.0029 20.5879 22.8808L37.9247 10.829L37.9509 29.0372L27.8841 21.2301C27.4373 20.8835 26.7935 20.9648 26.4468 21.4118C26.1 21.8589 26.1814 22.5025 26.6285 22.8492Z"
+                                  fill="#03503B"/>
+                        </svg>
+                    </div>
+                    <div class="card_email">
+                        GalInf@gmail.com
+                    </div>
+                </div>
+            </div>
+            <div class="text2">
+                The work does not require any special installation
+                Any work of Olena Chornohuz never have been exhibited in the Greater Toronto Area before
+            </div>
+            <div class="form">
+                <form action="./contacts.html" target="_blank" method="POST">
+            <span class="first">
+                Мы с вами свяжемся
+            </span>
+                    <div class="name">
+                        <input id="name" name="user_name" type="text" placeholder="Имя" required>
+                    </div>
+                    <div class="number_form">
+                        <input name="tel" type="tel" pattern="^\+38\d{3}\d{3}\d{2}\d{2}$"
+                               maxlength="13" placeholder="+38(000)000 00 00" required>
+                    </div>
+                    <span class="second">
+                поля обязательны к заполнению
+            </span>
+                    <div class="button">
+                        <button type="submit">
+                            Отправить
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    .content{
+        height: 530px;
+        width: 650px;
+    }
+    .main_content{
+        height: 600px;
+    }
+    .text2{
+        display: none;
+    }
+    .feedback_img{
+        display: none;
+    }
+    @media all and (max-width: 990px){
+        .main_content{
+            overflow: hidden;
+        }
+        .feedback_img{
+            display: block;
+        }
+        .left, .right{
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+        .main_content{
+            height: 100%;
+        }
+        .content{
+            width: 90%;
+            height: 80%;
+        }
+        .left{
+            width: 386px;
+        }
+        .right{
+            width: 261px;
+        }
+        .img img, .img{
+            width: 386px;
+            height: 386px;
+        }
+        .cards{
+            height: 233px;
+        }
+        .text{
+            display: none;
+        }
+        .text2{
+            width: 228px;
+            height: 109px;
+            text-align: center;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 14px;
+            line-height: 19px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #03503B;
+        }
+        .form{
+            margin-top: 0;
+            margin-bottom: 70px;
+        }
+    }
+    @media all and (max-width: 500px){
+        .left{
+            height: 269px;
+        }
+        .right{
+            justify-content: space-around;
+            height: 550px;
+            width: 269px;
+        }
+        body{
+            height: 100%;
+            width: 100%;
+        }
+        main{
+            height: 140vh;
+            width: 100%;
+        }
+        .main_content{
+            height: 100%;
+            width: 100%;
+            overflow: hidden;
+        }
+
+        .slick-list{
+            width: 100%;
+            height: 100%;
+        }
+        .card1, .card2, .card3{
+            width: 269px;
+            height: 71px;
+        }
+        .cards{
+            margin-top: 10%;
+            width: 269px;
+        }
+        .container{
+            margin-bottom: 24px;
+        }
+        .feedback_img{
+            display: none;
+        }
+        .img img, .img{
+            width: 269px;
+            height: 269px;
+        }
+        .content{
+            height: 1050px;
+            width: 100%;
+            flex-direction: column;
+            align-items: center;
+            justify-content: flex-start;
+        }
+        .text2{
+            order: 1;
+            font-weight: 300;
+            font-size: 12px;
+            line-height: 13px;
+            width: 269px;
+            height: 45px;
+        }
+        .form{
+            margin: 0;
+        }
+    }
+    @media all and (min-width: 340px) and (max-width: 385px){
+        main{
+            height: 150vh;
+        }
+    }
+    @media all and (max-width: 320px){
+        main{
+            height: 170vh;
+        }
+    }
+    @media all and (min-width: 400px) and (max-width: 420px){
+        main{
+            height: 120vh;
+        }
+    }
+    @media all and (max-width: 420px) and (max-height: 740px){
+        main{
+            height: 140vh;
+        }
+    }
+    @media all and (min-width: 340px) and (max-width: 385px){
+        main{
+            height: 160vh;
+        }
+    }
+    @media all and (height: 812px) and (width: 375px){
+        main{
+            height: 120vh;
+        }
+    }
+    @media all and (max-width: 823px) and (max-height: 411px){
+        .left{
+            height: 269px;
+        }
+        .right{
+            justify-content: space-around;
+            height: 550px;
+            width: 269px;
+        }
+        body{
+            height: 100%;
+            width: 100%;
+        }
+        main{
+            height: 270vh;
+            width: 100%;
+        }
+        .main_content{
+            height: 100%;
+            width: 100%;
+            overflow: hidden;
+        }
+
+        .slick-list{
+            width: 100%;
+            height: 100%;
+        }
+        .card1, .card2, .card3{
+            width: 269px;
+            height: 71px;
+        }
+        .cards{
+            margin-top: 10%;
+            width: 269px;
+        }
+        .container{
+            margin-bottom: 24px;
+        }
+        .feedback_img{
+            display: none;
+        }
+        .img img, .img{
+            width: 269px;
+            height: 269px;
+        }
+        .content{
+            height: 1050px;
+            width: 100%;
+            flex-direction: column;
+            align-items: center;
+            justify-content: flex-start;
+        }
+        .text2{
+            order: 1;
+            font-weight: 300;
+            font-size: 12px;
+            line-height: 13px;
+            width: 269px;
+            height: 45px;
+        }
+        .form{
+            margin: 0;
+        }
+    }
+    @media all and (max-width: 600px) and (max-height: 350px){
+        main{
+            height: 300vh;
+        }
+    }
+</style>
